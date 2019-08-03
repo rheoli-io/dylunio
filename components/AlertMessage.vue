0bd687a077ab2745ed094266acd2f68d30321243
CoolGoose <melipse@gmail.com>
2019-08-03 23:10:08 +0300

    cleanup and dependency updates

---
+++ components/AlertMessage.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <template v-for="(messages, messageType) in alertMessages">
+      <v-alert
+        v-for="(message, index) in messages"
+        v-bind:key="index"
+        :value="true"
+        :type="messageType"
+      >
+        {{ message }}
+      </v-alert>
+    </template>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      alertMessages: null
+    };
+  },
+  mounted() {
+    if (this.$store.state.alertMessages) {
+      this.alertMessages = this.$store.state.alertMessages;
+      this.$store.commit("clearAlertMessages");
+    }
+  }
+};
+</script>
---
+++ pages/password-reset.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>here</div>
+</template>
+
+<script>
+export default {
+  layout: "anonymous",
+  auth: false
+};
+</script>
+
+<style>
+</style>
